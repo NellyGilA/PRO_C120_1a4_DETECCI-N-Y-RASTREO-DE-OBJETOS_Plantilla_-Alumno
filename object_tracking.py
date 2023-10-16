74ee9f240b041a8855fa79c20ee904f35b00a867
NellyGilA <86023328+NellyGilA@users.noreply.github.com>
2023-10-16 16:25:55 -0600

    Update object_tracking.py

--- object_tracking.py
+++ object_tracking.py
@@ -4,7 +4,7 @@ import math
 
 video = cv2.VideoCapture("bb3.mp4")
 
-# Carga el rastreador 
+# Carga el rastreador
 tracker = cv2.TrackerCSRT_create()
 
 # Lee el primer cuadro del video
@@ -18,23 +18,37 @@ tracker.init(img, bbox)
 
 print(bbox)
 
+def drawBox(img, bbox):
+    x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
 
+    cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
+
+    cv2.putText(img,"Rastreando",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
+
+def goal_track(img, bbox):
+    x, y,w, h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
 
 while True:
-    check,img = video.read()   
+    
+    check, img = video.read()   
 
-    cv2.imshow("resultado",img)
+    # Actualiza el rastreador en la imagen y el cuadro delimitador
+    success, bbox = tracker.update(img)
+
+    if success:
+        drawBox(img, bbox)
+    else:
+        cv2.putText(img,"Perdido",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+
+    cv2.imshow("resultado", img)
             
     key = cv2.waitKey(25)
-
     if key == 32:
-        print("¡Alto!")
+        print("Alto")
         break
-
 
 video.release()
 cv2.destroyALLwindows()
-
 
 
 
